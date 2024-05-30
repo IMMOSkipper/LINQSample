217ab88784ca161be9ab0890cd91bf8f79028c60
IMMOSkipper <48646643+IMMOSkipper@users.noreply.github.com>
2024-05-30 08:56:55 +0800

    Add files via upload

---
+++ LINQSample/LINQSample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 5, 28, 31, 17, 16, 42 };
+
+            var numsQuery = from n in numbers
+                           where n < 20
+                           select n;
+
+            var numsMethod = numbers.Where(N => N < 20);
+
+            int numsCount = (from n in numbers
+                             where n < 20
+                             select n).Count();
+
+            foreach(var x in numsQuery)
+                Console.Write($"{ x }, ");
+            Console.WriteLine();
+
+            foreach (var x in numsMethod)
+                Console.Write($"{ x }, ");
+            Console.WriteLine();
+
+            Console.WriteLine(numsCount);
+        }
+    }
+}
